9973e095e0e3dea92725d4a5da301ab603c112ff
Dean Cook <deanrobertcook@gmail.com>
2021-12-23 16:55:18 +0100

    Setup react

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/react.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './www',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'www'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ],
+  },
+};
