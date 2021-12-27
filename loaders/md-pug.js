f6ae37d96eada91a94f03c2532cc9d67b1a9f881
Dean Cook <deanrobertcook@gmail.com>
2021-12-27 16:06:55 +0100

    Get webpack compiling single md and pug template

---
+++ loaders/md-pug.js
@@ -0,0 +1,60 @@
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Williams Medina @willyelm
+
+  Taken from: https://github.com/willyelm/pug-html-loader/blob/master/lib/index.js
+*/
+'use strict'
+const path = require('path');
+const pug = require('pug');
+const md = require('markdown-it')({ html: true });
+const yaml = require('yaml');
+const datefmt = require('date-fns');
+
+let cachedDeps;
+
+const formatDate = (dateStr) => datefmt.format(new Date(dateStr), 'MMM. yyyy');
+
+module.exports = function (source) {
+
+
+  let p = path.resolve('./src/post.pug');
+  this.addDependency(p);
+
+  let { content, frontMatter } = splitFrontMatter(source);
+
+  let template = pug.compileFile('./src/post.pug', {
+    filename: this.resourcePath,
+    doctype: this.query.doctype || 'js',
+    compileDebug: this.debug || false,
+  });
+
+  if (this.cacheable) {
+    this.cacheable(true)
+  }
+
+  cachedDeps = template.dependencies ? template.dependencies.slice() : undefined;
+  template.dependencies.forEach(this.addDependency);
+  let data = this.query.data || {}
+  return template({
+    pretty: true,
+    slug: 'about',
+    href: 'about.html',
+    content: md.render(content),
+    ...frontMatter,
+    formatDate
+  })
+}
+
+function splitFrontMatter(fileContent) {
+  let sections = fileContent.split('---');
+  if (fileContent.indexOf('---') == 0 && sections.length > 2) {
+    return {
+      frontMatter: yaml.parse(sections[1]),
+      content: sections.slice(2).join('---')
+    }
+  }
+  return {
+    content: fileContent
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs
+
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    css: require.resolve("./src/style.css"),
+    js: './src/main.js'
+  },
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
@@ -10,9 +17,37 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'www'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'content/about.md',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      }
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
+      {
+        test: /\.md$/i,
+        use: [
+          "html-loader",
+          {
+            loader: path.resolve('loaders', 'md-pug.js'),
+            options: {
+              test: "test"
+            }
+          }]
+      },
+      {
+        test: require.resolve("./src/style.css"),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
       {
         test: /\.js$/i,
         exclude: /(node_modules|bower_components)/,
@@ -22,7 +57,38 @@ module.exports = {
             presets: ['@babel/preset-react']
           }
         }
-      }
+      },
     ],
   },
 };
+
+function htmlPlugins() {
+  var posts = [];
+  fs.readdirSync('content').forEach(file => {
+    let slug = file.slice(0, file.lastIndexOf('.md'));
+    let href = slug + '.html';
+
+    let { content, frontMatter } = splitFrontMatter(fs.readFileSync(`content/${file}`, 'utf8'));
+
+    if (frontMatter['exclude']) {
+      return;
+    }
+
+    posts.push({
+      slug,
+      href,
+      preview: extractPreview(content),
+      ...frontMatter
+    });
+
+    fs.writeFileSync(`www/${slug}.html`, pug.renderFile('src/post.pug', {
+      pretty: true,
+      slug,
+      href,
+      content: md.render(content),
+      ...frontMatter,
+      formatDate
+    }));
+  });
+  return posts;
+}
