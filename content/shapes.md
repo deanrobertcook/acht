d937b11c4da4514331d7ea5829ddaff4d3750800
Dean Cook <deanrobertcook@gmail.com>
2022-01-22 12:42:33 +0100

    Add findings about bezier extrema

--- content/shapes.md
+++ content/shapes.md
@@ -16,3 +16,16 @@ So, I started off trying to do this using three.js since I want to quickly get i
 I'm a big fan of the [coding train](https://thecodingtrain.com/) and have wanted to follow along some of his courses for a while. He uses p5.js, so I dove in and turns out you can get 2D animations up with much less boiler plate code. They've done a really good job of making it user friendly, so let's stick to that for now:
 
 <div style="height: 500px" id="bezier-p5"></div>
+
+### Finding the extremum
+Bezier curves are defined by the anchor and control points around which the linear interpolation occurs. But what if we want to know exactly how much the curve itself buldges out? I.e, how would we find the maximum/minimum of the curve. Sticking to quadratic curves to make things easy:
+
+<div style="height: 500px" id="bezier-extremum"></div>
+
+It turns out, after doing the derivation, that the extremum is always at the position `t = 0.5`. Or another way of putting it, the middle most point (or points) will the be the highest/lowest point on the curve. We can then just plug `0.5` into our bezier function to find it's value at that point.
+
+Interestingly, if we draw a line between the middle of the curve and the control point, the line seems to intersect our bezier curve at exactly the extrema (TODO - prove this).
+
+
+
+
--- src/js/p5fun.js
+++ src/js/p5fun.js
@@ -1,13 +1,18 @@
 import p5 from 'p5';
 
 addEventListener('load', (event) => {
-  const root = document.getElementById('bezier-p5');
-  if (root) {
-    init(root);
+  const bezierP5 = document.getElementById('bezier-p5');
+  if (bezierP5) {
+    bezierP5Init(bezierP5);
+  }
+
+  const bezierExtremum = document.getElementById('bezier-extremum');
+  if (bezierExtremum) {
+    bezierExtremumInit(bezierExtremum);
   }
 });
 
-function init(div) {
+function bezierP5Init(div) {
   new p5((/** @type {p5} */s) => {
 
     let p0, p1, cp1, cp2;
@@ -48,5 +53,78 @@ function init(div) {
       s.endShape();
     };
   }, 'bezier-p5');
+}
+
+function bezierExtremumInit(div) {
+  new p5((/** @type {p5} */s) => {
+    const STEPS = 10;
+
+    let ap1, ap2, cp;
+  
+    s.setup = () => {
+      s.createCanvas(div.offsetWidth, div.offsetHeight);
+      ap1 = s.createVector(3, s.height / 2);
+      ap2 = s.createVector(s.width - 3, s.height / 2);
+      cp = s.createVector(0, 0);
+    };
+  
+    const POINTS = 10;
+    s.draw = () => {
+      s.clear();
+      s.stroke(0);
+      s.strokeWeight(2);
+
+      s.push();
+      s.stroke(0);
+      s.strokeWeight(6);
+      s.point(s.width / 2, s.height / 2);
+      s.pop();
+      
+      s.line(s.width/2, s.height / 2, cp.x, cp.y);
+      
+      cp.set(s.mouseX, s.mouseY);
+      
+      let extT = findExtremum(ap1.y, cp.y, ap2.y);
+      
+      let extX = bezier1D(ap1.x, cp.x, ap2.x, extT);
+      let extY = bezier1D(ap1.y, cp.y, ap2.y, extT);
+
+      s.push();
+      s.strokeWeight(0);
+      s.text(`(${Math.round(extX)}, ${Math.round(extY)})`, extX - 10, extY - 10);
+      s.text(`(${Math.round(cp.x)}, ${Math.round(cp.y)})`, cp.x - 10, cp.y - 10);
+      s.pop();
+
+      s.push();
+      s.stroke(0, 255, 0);
+      s.line(0, extY, s.width, extY);
+      s.pop();
+      
+      for (let t = 0; t <= 1.00001; t += 1 / STEPS) {
+        let p1 = p5.Vector.lerp(ap1, cp, t);
+        let p2 = p5.Vector.lerp(cp, ap2, t);
+        //line(p1.x, p1.y, p2.x, p2.y);
+        
+        let px = bezier1D(ap1.x, cp.x, ap2.x, t);
+        let py = bezier1D(ap1.y, cp.y, ap2.y, t);
+        
+        s.push();
+        s.stroke(255, 0, 0);
+        s.strokeWeight(6);
+        s.point(px, py);
+        s.pop();
+
+      }
+    };
+  }, 'bezier-extremum');
+}
+
+function bezier1D(ap1, cp, ap2, t) {
+  return ap1 + 2*t*(cp - ap1) + t*t*(ap2 - 2*cp + ap1);
+}
+
+//turns out it's always t = 0.5!
+function findExtremum(a, b, c) {
+  return (a - b) / (c - 2*b + a);
 }
 
