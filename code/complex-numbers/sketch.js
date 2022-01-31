301a7878ab1415eca861fc06c4379dd32e721d17
Dean Cook <deanrobertcook@gmail.com>
2022-01-31 17:58:44 +0100

    Make cartesian coordinates more flexible

--- code/complex-numbers/sketch.js
+++ code/complex-numbers/sketch.js
@@ -6,31 +6,25 @@ function draw() {
   noFill();
   background(51);
   stroke(255);
-  
+
   let w = 32;
-  let h = w * (width / height);
+  let h = w * (height / width);
   let scl = width / w;
 
   setupCartesianCoordinates(w, h, scl);
 
+  strokeWeight(1 / scl);
+  stroke(255, 0, 0);
 
-  strokeWeight(2 / scl);
+  let z1 = [4, 3];
+  let [a, b] = z1;
+  let z2 = [0, 1];
+  let [c, d] = z1;
 
-  point(0, 0);
-  line(0, 0, 4, 3);
-  // let x1 = createVector(width / 2, height / 2);
-  // let x2 = createVector(mouseX, mouseY);
+  drawZ(z1);
+  drawZ(z2);
 
-
-
-  // // this code is to make the arrow point
-  // push() //start new drawing state
-  // var angle = atan2(x1.y - x2.y, x1.x - x2.x); //gets the angle of the line
-  // translate(x2.x, x2.y); //translates to the destination vertex
-  // rotate(angle-HALF_PI); //rotates the arrow point
-  // triangle(-offset*0.5, offset, offset*0.5, offset, 0, -offset/2); //draws the arrow point as a triangle
-  // pop();
-
+  drawZ(multC(z1, z2));
 }
 
 function setupCartesianCoordinates(w, h, scl) {
@@ -38,25 +32,21 @@ function setupCartesianCoordinates(w, h, scl) {
   line(width / 2, 0, width / 2, height);
   line(0, height / 2, width, height / 2);
 
-  const pointSize = 2;
-  //draw x coordinate
-  push();
-  strokeWeight(pointSize);
-  translate(0, height / 2);
-  for (let i = 0; i <= w; i++) {
-    point(i * scl, 0);
-  }
-  pop();
-
-  //draw y coordinate
-  push();
-  strokeWeight(pointSize);
-  translate(width / 2, 0);
-  for (let i = 0; i <= h; i++) {
-    point(0, (i - 0.5) * scl); //TODO make more generic in case of odd numbers
-  }
-  pop();
-
   translate(width / 2, height / 2);
   scale(scl, -scl);
+  push();
+  strokeWeight(2 / scl);
+  for (let i = floor(-w / 2); i <= ceil(w / 2); i++) {
+    point(i, 0);
+  }
+  for (let i = floor(-h / 2); i <= ceil(h / 2); i++) {
+    point(0, i);
+  }
+  pop();
+
+}
+
+function drawZ(z) {
+  let [a, b] = z;
+  line(0, 0, a, b);
 }
