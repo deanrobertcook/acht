a0c8a38ac4629514c841ae4803c9aee5f36254b9
Dean Cook <deanrobertcook@gmail.com>
2022-06-01 08:13:07 +0200

    Create draggable lines

--- code/projective-conics/sketch.js
+++ code/projective-conics/sketch.js
@@ -15,48 +15,45 @@ let o1;
 function setup() {
   createCanvas(500, 500);
 
-  l1 = Line.fromPoints(new p5.Vector(width/4, 0), new p5.Vector(width/2, height));
-  l2 = Line.fromPoints(new p5.Vector(width/8, height), new p5.Vector(width/4, height/4));
-  l3 = Line.fromPoints(new p5.Vector(0, 2*height/3), new p5.Vector(width, height/3));
+  l1 = new Line(width / 4, 0, width / 2, height, "l1");
+  l2 = new Line(width / 8, height, width / 4, height / 4, "l2");
+  l3 = new Line(0, 2 * height / 3, width, height / 3, "l3");
 
 
   p1 = new LineDraggablePoint(l1, r, "p1");
-  
-  o1 = new DraggablePoint(width/8, height/8, r, "o1");
-  o2 = new DraggablePoint(3*width/4, 3*height/4, r, "o2");
 
-  draggables.push(p1, o1, o2);
-  
+  o1 = new DraggablePoint(width / 8, height / 8, r, "o1");
+  o2 = new DraggablePoint(3 * width / 4, 3 * height / 4, r, "o2");
+
+  draggables.push(p1, o1, o2, l1, l2, l3);
+
 }
 
 function draw() {
   background(255);
 
-  let m1 = Line.fromPoints(o1, p1);
-  let p2 = l2.findIntersectsWithLine(m1);
-  p2.draw();
-  
-  let m2 = Line.fromPoints(o2, p2);
-  let p3 = l3.findIntersectsWithLine(m2);
-  p3.draw();
+  // let m1 = Line.fromPoints(o1, p1);
+  // let p2 = l2.findIntersectsWithLine(m1);
+  // p2.draw();
 
-  let m3 = Line.fromPoints(p1, p3);
+  // let m2 = Line.fromPoints(o2, p2);
+  // let p3 = l3.findIntersectsWithLine(m2);
+  // p3.draw();
 
-  [m1, m2, m3].forEach(l => { 
-    push();
-    setLineDash([5, 5])
-    l.draw(); 
-    pop();
-  });
+  // let m3 = Line.fromPoints(p1, p3);
+
+  // [m1, m2, m3].forEach(l => { 
+  //   push();
+  //   setLineDash([5, 5])
+  //   l.draw(); 
+  //   pop();
+  // });
 
   draggables.forEach(p => {
     p.over();
     p.update();
     p.draw();
   });
-  [l1, l2, l3].forEach(l => { 
-    l.draw(); 
-  }); 
 }
 
 function setLineDash(list) {
@@ -86,14 +83,25 @@ class Point {
     this.i = i;
   }
 
-  draw() {
-    push()
+  drawText() {
+    if (this.i) {
+      textSize(9);
+      text(this.i, this.x + this.r, this.y - this.r);
+    }
+  }
+
+  drawCircle() {
     fill(175, 200);
     circle(this.x, this.y, this.r * 2);
+  }
+
+  draw() {
+    push()
+    this.drawCircle();
     pop();
-    if (this.i) {
-      text(this.i, this.x, this.y)
-    }
+    push();
+    this.drawText();
+    pop();
   }
 }
 class DraggablePoint extends Point {
@@ -105,14 +113,14 @@ class DraggablePoint extends Point {
     this.offsetY = 0;
   }
 
-  over() {    
+  over() {
     let r = this.r;
 
     let xdist = mouseX - this.x;
     let ydist = mouseY - this.y;
 
     // Is mouse over object
-    if (r*r > xdist*xdist + ydist*ydist) {
+    if (r * r > xdist * xdist + ydist * ydist) {
       this.rollover = true;
     } else {
       this.rollover = false;
@@ -127,9 +135,7 @@ class DraggablePoint extends Point {
     }
   }
 
-  draw() {
-    // Different fill based on state
-    push()
+  drawCircle() {
     if (this.dragging) {
       fill(50);
     } else if (this.rollover) {
@@ -138,12 +144,6 @@ class DraggablePoint extends Point {
       fill(175, 200);
     }
     circle(this.x, this.y, this.r * 2);
-    pop();
-    if (this.i) {
-
-      text(this.i, this.x, this.y)
-    }
-    
   }
 
   pressed() {
@@ -153,7 +153,7 @@ class DraggablePoint extends Point {
     let ydist = mouseY - this.y;
 
     // Did I click on the object?
-    if (r*r > xdist*xdist + ydist*ydist) {
+    if (r * r > xdist * xdist + ydist * ydist) {
       this.dragging = true;
       // If so, keep track of relative location of click to center of cirlce
       this.offsetX = this.x - mouseX;
@@ -170,11 +170,11 @@ class DraggablePoint extends Point {
 class LineDraggablePoint extends DraggablePoint {
   constructor(l, r, i) {
     let [a, b, c] = [l.a, l.b, l.c];
-    let [cx, cy] = [width/2, height/2];
+    let [cx, cy] = [width / 2, height / 2];
 
-    let d = a*cy - b*cx;
-    let y = (a*d - b*c) / (a*a + b*b);
-    let x = (-b*d - a*c) / (a*a + b*b);
+    let d = a * cy - b * cx;
+    let y = (a * d - b * c) / (a * a + b * b);
+    let x = (-b * d - a * c) / (a * a + b * b);
 
     super(x, y, r, i);
     this.l = l;
@@ -185,45 +185,58 @@ class LineDraggablePoint extends DraggablePoint {
     if (this.dragging) {
       let [a, b] = [this.l.a, this.l.b];
       let [x, y] = [this.x, this.y];
-    
-      let dot = b*(mouseX - x) - a*(mouseY - y);
-      let den = a*a + b*b;
 
-      this.x = b*dot/den + x;
-      this.y = -a*dot/den + y;
+      let dot = b * (mouseX - x) - a * (mouseY - y);
+      let den = a * a + b * b;
+
+      this.x = b * dot / den + x;
+      this.y = -a * dot / den + y;
     }
   }
 }
 
 class Line {
-  constructor(a, b, c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
+
+  //TODO - figure out a better way of drawing points, some might be drawn twice
+  //depending on how they are kept track of in the draw method
+  constructor(x1, y1, x2, y2, i) {
+    if (x1 <= 0)          { x1 += 10 }
+    else if (x1 >= width) { x1 -= 10 }
+    if (x2 <= 0)          { x2 += 15 }
+    else if (x2 >= width) { x2 -= 15 }
+    if (y1 <= 0)           { y1 += 10 }
+    else if (y1 >= height) { y1 -= 10 }
+    if (y2 <= 0)           { y2 += 15 }
+    else if (y2 >= height) { y2 -= 15 }
+    this.cp1 = new DraggablePoint(x1, y1, r, i);
+    this.cp2 = new DraggablePoint(x2, y2, r, i);
   }
 
-  static fromPoints(p1, p2) {
-    let a = p1.y - p2.y;
-    let b = p2.x - p1.x;
-    let c = (p1.x * p2.y) - (p2.x * p1.y);
-    return new Line(a, b, c);
+  static fromPoints(p1, p2, i) {
+    return new Line(p1.x, p1.y, p2.x, p2.y, i);
+  }
+
+  getCoefficients() {
+    let [cp1, cp2] = [this.cp1, this.cp2];
+    let a = cp1.y - cp2.y;
+    let b = cp2.x - cp1.x;
+    let c = (cp1.x * cp2.y) - (cp2.x * cp1.y);
+    return [a, b, c];
   }
 
   findIntersectsWithCircle(r) {
-    let a = this.a;
-    let b = this.b;
-    let c = this.c;
+    let [a, b, c] = this.getCoefficients();
 
-    let x0 = -a*c / (a*a + b*b)
-    let y0 = -b*c / (a*a + b*b)
-  
-    if (c*c > r*r * (a*a + b*b) + EPS) {
+    let x0 = -a * c / (a * a + b * b)
+    let y0 = -b * c / (a * a + b * b)
+
+    if (c * c > r * r * (a * a + b * b) + EPS) {
       return [];
-    } else if (Math.abs(c*c - r*r * (a*a + b*b)) < EPS) {
+    } else if (Math.abs(c * c - r * r * (a * a + b * b)) < EPS) {
       return [new p5.Vector(x0, y0)]
     } else {
-      let d = r*r - c*c/(a*a+b*b);
-      let mult = Math.sqrt (d / (a*a+b*b));
+      let d = r * r - c * c / (a * a + b * b);
+      let mult = Math.sqrt(d / (a * a + b * b));
       let ax = x0 + b * mult;
       let bx = x0 - b * mult;
       let ay = y0 - a * mult;
@@ -233,24 +246,46 @@ class Line {
   }
 
   findIntersectsWithLine(l2) {
-    let l1 = this;
-    let x = - (l1.c * l2.b - l2.c * l1.b) / (l1.a * l2.b - l2.a * l1.b);
-    let y = - (l1.a * l2.c - l2.a * l1.c) / (l1.a * l2.b - l2.a * l1.b);
+    let [a1, b1, c1] = this.getCoefficients();
+    let [a2, b2, c2] = l2.getCoefficients();
+    let x = - (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+    let y = - (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
     return new Point(x, y, r);
+  }
+
+  over() {
+    this.cp1.over();
+    this.cp2.over();
+  }
+
+  update() {
+    this.cp1.update();
+    this.cp2.update();
+  }
+
+  pressed() {
+    this.cp1.pressed();
+    this.cp2.pressed();
+  }
+
+  released() {
+    this.cp1.released();
+    this.cp2.released();
   }
 
   /**
    * Draw the line as far as possible on the canvas
    */
   draw() {
+    this.cp1.draw();
+    this.cp2.draw();
+    let [a, b, c] = this.getCoefficients();
     push();
-    // translate(width / 2, height / 2);
-    // scale(1, -1);
     let x0 = 0;
-    let y0 = (-this.a * x0 - this.c) / this.b
+    let y0 = (-a * x0 - c) / b
 
     let x1 = width;
-    let y1 = (-this.a * x1 - this.c) / this.b
+    let y1 = (-a * x1 - c) / b
     line(x0, y0, x1, y1);
     pop();
   }
